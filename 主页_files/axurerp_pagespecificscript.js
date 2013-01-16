c8a91534e7437134acb577e389999b5709fbb2d6
Ryan <cumt.xiaochi@gmail.com>
2013-01-16 15:25:42 +0800

    first commit

---
+++ 主页_files/axurerp_pagespecificscript.js
@@ -0,0 +1,108 @@
+﻿for(var i = 0; i < 70; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u7'] = 'top';document.getElementById('u51_img').tabIndex = 0;
+
+u51.style.cursor = 'pointer';
+$axure.eventManager.click('u51', function(e) {
+
+if (true) {
+
+	ScrollToWidget('u54', false,true,'linear',500);
+
+}
+});
+
+$axure.eventManager.mouseover('u51', function(e) {
+if (!IsTrueMouseOver('u51',e)) return;
+if (true) {
+
+	SetPanelVisibility('u48','','fade',500);
+
+}
+});
+gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u16'] = 'center';document.getElementById('u46_img').tabIndex = 0;
+
+u46.style.cursor = 'pointer';
+$axure.eventManager.click('u46', function(e) {
+
+if (true) {
+
+	ScrollToWidget('u53', false,true,'linear',500);
+
+}
+});
+
+$axure.eventManager.mouseover('u46', function(e) {
+if (!IsTrueMouseOver('u46',e)) return;
+if (true) {
+
+	SetPanelVisibility('u39','','fade',500);
+
+}
+});
+gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u62'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u30'] = 'top';u60.tabIndex = 0;
+
+u60.style.cursor = 'pointer';
+$axure.eventManager.click('u60', function(e) {
+
+if (true) {
+
+	NewTab('http://weibo.com/fanwei0212', "");
+
+}
+});
+gv_vAlignTable['u34'] = 'top';u64.tabIndex = 0;
+
+u64.style.cursor = 'pointer';
+$axure.eventManager.click('u64', function(e) {
+
+if (true) {
+
+	SetPanelState('u0', 'pd1u0','none','',500,'swing','up',1000);
+
+}
+});
+gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u58'] = 'center';
+$axure.eventManager.mouseout('u49', function(e) {
+if (!IsTrueMouseOut('u49',e)) return;
+if (true) {
+
+	SetPanelVisibility('u48','hidden','none',500);
+
+}
+});
+gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u66'] = 'center';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u52'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u24'] = 'top';document.getElementById('u4_img').tabIndex = 0;
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	SetPanelState('u0', 'pd0u0','none','',500,'swing','down',1000);
+
+}
+});
+gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u56'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u67'] = 'top';gv_vAlignTable['u10'] = 'top';
+$axure.eventManager.mouseout('u40', function(e) {
+if (!IsTrueMouseOut('u40',e)) return;
+if (true) {
+
+	SetPanelVisibility('u39','hidden','fade',500);
+
+}
+});
+gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u29'] = 'top';u59.tabIndex = 0;
+
+u59.style.cursor = 'pointer';
+$axure.eventManager.click('u59', function(e) {
+
+if (true) {
+
+	NewTab('http://www.weibo.com/138139898', "");
+
+}
+});
